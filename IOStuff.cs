2526b7fda08df83025177cb1299ee3495a2a1045
Ron Studený <ronstudeny@gmail.com>
2023-02-21 15:17:14 +0100

    whatever

---
+++ IOStuff.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Hodina
+{
+    public class IOStuff
+    {
+        public string Path { get; set; }
+
+        private string pattern;
+
+        IOStuff(string address)
+        {
+            Path = address;
+        }
+
+        private void ReadFromFile()
+        {
+            string[] fileRead;
+
+            if (File.Exists(Path))
+            {
+                fileRead = File.ReadAllLines(Path);
+            }
+  
+        }
+
+
+    }
+}
